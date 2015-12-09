eaa7e70755f599089b4f4a89b93d9661c54bce84
jemarsha <jmarsha5@nd.edu>
2015-12-09 14:57:08 -0500

    Update tweet_clustering.py

--- tweet_clustering.py
+++ tweet_clustering.py
@@ -3,17 +3,15 @@ import math
 import string
 import itertools
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import codecs
 
-################################################################################
-#
-#	AUXILIARY FUNCTIONS
-#	The functions below are employed by the k-means algorithm.
-#
+
+
+
+
+
 #	jaccard_similarity is used to calculate the distance between tweets.
-#
-################################################################################
+
 
 def jaccard_distance(x, y):
 	"""Returns the Jaccard distance (or 1-similarity) between two lists"""
@@ -21,12 +19,10 @@ def jaccard_distance(x, y):
 	union_cardinality = len(set.union(*[set(x), set(y)]))
 	return 1-intersection_cardinality/float(union_cardinality)
  
-################################################################################
-#
+
 #	K-MEANS CLUSTERING
-#	The functions below implement k-means clustering for a text corpus. 
-#
-################################################################################
+
+
 
 MAX_ITERATIONS = 1000
 
@@ -97,33 +93,88 @@ def kmeans(dataSet, initial_centroids):
 	set of centroids. Returns the same dataSet with each dataPoint labeled by 
 	the ID of its cluster's centroid."""
     # Initialize centroids randomly
-    k = len(initial_centroids)
-    centroids = initial_centroids
-    
+	k = len(initial_centroids)
+	centroids = initial_centroids
+
     # Initialize book keeping vars.
-    iterations = 0
-    oldCentroids = None
-    
+	iterations = 0
+	oldCentroids = None
+
     # Run the main k-means algorithm
-    while not shouldStop(oldCentroids, centroids, iterations):
-        # Save old centroids for convergence test. Book keeping.
-        oldCentroids = centroids
-        iterations += 1
-        
-        # Assign labels to each datapoint based on centroids
-        dataSet = setLabels(dataSet, centroids)
-        
-        # Assign centroids based on datapoint labels
-        centroids = getCentroids(dataSet)
-        
-    # We can get the labels too by calling getLabels(dataSet, centroids)
-    return dataSet
+	while not shouldStop(oldCentroids, centroids, iterations):
+		# Save old centroids for convergence test. Book keeping.
+		oldCentroids = centroids
+		iterations += 1
+
+		# Assign labels to each datapoint based on centroids
+		dataSet = setLabels(dataSet, centroids)
+
+		# Assign centroids based on datapoint labels
+		centroids = getCentroids(dataSet)
+
+	# We can get the labels too by calling getLabels(dataSet, centroids)
+	return dataSet
+
+
+def getRoot(tweet_id, tweets):
+	""" Finds the root of the forest of this tweet using path compression """
+	if tweets[tweet_id]['centroid'] != tweet_id:
+		tweets[tweet_id]['centroid'] = getRoot(tweets[tweet_id]['centroid'], tweets)
+
+	return tweets[tweet_id]['centroid']
+
+
+def initCentroids(tweets, max_dist):
+	""" Initializes the initial centroids based on Kruskal's algorithm by creating
+	forests of minimum spanning trees and distances within each forest up to
+	max_dist """
+
+	distances = []
+	
+	for key1 in tweets.keys():
+		tweets[key1]['centroid'] = key1
+		for key2 in tweets.keys():
+			if key1 < key2:
+				distances.append([key1, key2, \
+					jaccard_distance(tweets[key1]['text'], tweets[key2]['text'])])
+
+	distances.sort(key = lambda distance: distance[2])
+
+	forests = len(tweets)
+
+	for [key1, key2, distance] in distances:
+		root1 = getRoot(key1, tweets)
+		root2 = getRoot(key2, tweets)
+		if root1 != root2:
+			if jaccard_distance(tweets[root1]['text'], tweets[root2]['text']) < max_dist:
+				tweets[root2]['centroid'] = getRoot(root1, tweets)
+				forests -= 1
+
+	# ensure that the centroid is the root of the forest
+	for key in tweets.keys():
+		getRoot(key, tweets)
+	
 
 if __name__ == '__main__':
+	count = 0
+	if len(sys.argv) != 5:
+		print("  Usage: python tweet_clustering.py <tweets> <max_dist>" + \
+			" <output> <human-output>")
+		print("Example: python tweet_clustering.py Tweets.json 0.7" + \
+			" output.txt output-for-humans.txt")
+
+		print("  Usage: python tweet_clustering.py <tweets> <seeds>" + \
+			" <output> <human-output>")
+		print("Example: python tweet_clustering.py Tweets.json" + \
+			" InitialSeeds.txt output.txt output-for-humans.txt")
+
+		sys.exit(0)
+
+
 	# Import and process tweets
-	with open('Tweets.json', 'r') as f:
+	with open(sys.argv[1], 'r') as f:
 		import json
-		# Our dataset is of the form:
+		# The dataset is of the form:
 		# {ID: {text: [list of tweet words]
 		#		centroidID: ID of tweet at centroid cluster
 		#	 }
@@ -139,29 +190,36 @@ if __name__ == '__main__':
 			tweet['text'] = ''.join(ch for ch in tweet['text'] if ch not in exclude)
 			# Convert to lowercase
 			tweet['text'] = tweet['text'].lower()
-			# Split into words and record it in our dataset
+			# Split into words and record it in the dataset
 			tweets[tweet['id']] = {'text': tweet['text'].split(' '),
 								   'centroid': None}
 
-	# Now that our data is living in a vector space, we can execute K-means.
-	# First, we load the suggested initial centroids:
-	with open('InitialSeeds.txt', 'r') as f:
-		centroids = []
-		for l in f.readlines():
-			ID = int(l.replace(',', ''))
-			centroids.append(ID)
+	# Depending on the command line argument, either load the suggested initial
+	# centroids from a file, or initialize them from my algorithm
+	try:
+		initCentroids(tweets, float(sys.argv[2]))
+		centroids = list(set([tweets[tweet_id]['centroid'] for tweet_id in tweets.keys()]))
+	except ValueError:
+		with open(sys.argv[2], 'r') as f:
+			centroids = []
+			for l in f.readlines():
+				ID = int(l.replace(',', ''))
+				centroids.append(ID)
 
-	# We run the classification algo
+	# I run the classification algo
 	tweets = kmeans(tweets, centroids)
 
 	# And save the output data
-	with open('output.txt', 'w') as f:
+	with open(sys.argv[3], 'w') as f:
 		clusters = getClusters(tweets)
 		for clusterID in clusters:
+            		count += 1
 			f.write(str(clusterID) + ": " + ', '.join(map(str, clusters[clusterID])) + '\n')
-	
-	# We also create a human-readable file to confirm our algo works. Yay! 
-	with open('output-for-humans.txt', 'w') as f:
+	print count
+        
+	    
+	# also create a human-readable file 
+	with codecs.open(filename=sys.argv[4], mode="w", encoding="utf-8") as f:
 		out = ''
 		for clusterID in clusters:
 			out += 'Centroid: ' +  tweets2[clusterID] + ':\n'
